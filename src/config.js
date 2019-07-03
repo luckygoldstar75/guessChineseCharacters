077e344a125bd38352d7adb76cdcb3f13a88d276
Yo Nel <yonel@yopmail.com>
2019-07-03 13:27:20 +0200

    slight modifications for heroku to work

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 import AWS from 'aws-sdk';
 
+var serverHost='localhost'||'0.0.0.0'; //0.0.0.0 is for heroku / localhost in dev mode
+
 AWS.config.update({
       accessKeyId: "AKIAI25TDQD76X2NL3QQ",
       secretAccessKey: "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",
@@ -9,7 +11,7 @@ AWS.config.update({
 
 const _myConfig = {
  "server" :  {  
-	host: 'localhost',
+	host: serverHost,
 	port: 4000,
 	privateKeyAuth : 'eb6195b4d1ebb0c1d336a38629f11871401ec0c19c950caa91275db57cccbdbf513d8fd1c5e60311a724406910238dbffeabe17cf36fbbddb2b158472b07b1fc',
 	cookieExpirationTimeMs : 3 * 24 * 60 * 60 * 1000, /* Set session to 3 days */
